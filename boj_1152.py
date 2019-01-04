18a944c40ab1e64655b0de3836442cbfc2a32877
donkim <dickings@likelion.org>
2019-01-04 10:41:03 +0900

    #1152 단어의 개수 : 1. 이건 솔직히 .. 파이썬만을 위한 문제...

---
+++ boj_1152.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  4 10:37:23 2019
+
+@author: donsdev
+"""
+
+#1152 단어의 개수 
+long_str = input().split()
+print(len(long_str))
