77a3818fbb70ef3c8c4da5925efdc8bd52a92694
donkim <dickings@likelion.org>
2019-02-06 22:18:49 +0900

    [Greedy] #1931 회의실 배정 1. 왠만하면 pop 같은거 함부로 쓰지 말자

--- greedy/boj_1931.py
+++ greedy/boj_1931.py
@@ -4,5 +4,37 @@
 Created on Wed Feb  6 21:37:38 2019
 
 @author: donsdev
+import sys 
+input = sys.stdin.readline
 """
 
+n = int(input())
+arr = []
+for _ in range(n):
+    a,b = map(int,input().split())
+    arr.append((b,a))
+arr.sort()
+
+cnt = 0
+tmp = 0
+
+for b,a in arr:
+    if a >= tmp:
+        tmp = b
+        cnt += 1
+print(cnt)
+
+"""
+11
+1 4
+3 5
+0 6
+5 7
+3 8
+5 9
+6 10
+8 11
+8 12
+2 13
+12 14
+"""
