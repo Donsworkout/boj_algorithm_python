ee37fd82c8e5cbdf3c631bcfe1492269ed2308ff
donkim <dickings@likelion.org>
2019-04-10 11:15:10 +0900

    인구이동 BFS 풀이 - 탐색 대비완료

--- samsung_sw/boj_16234.py
+++ samsung_sw/boj_16234.py
@@ -1,66 +1,61 @@
-import collections
+from collections import deque
 import sys
 input = sys.stdin.readline
-moves = [(1,0),(-1,0),(0,1),(0,-1)]
+dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
 def valid_range(a, b):
-    global n
-    if a >= n or b >= n or a < 0 or b < 0:
+    if a < 0 or b < 0 or a >=n or b >=n:
         return False
     else:
         return True
 
 
 def BFS():
-    c, p = 0, 0
-    while queue:
+    global switch
+    while(queue):
         len_q = len(queue)
         for _ in range(len_q):
             cur = queue.popleft()
-            x, y = cur[0], cur[1]
-            for move in moves:
-                cx, cy = x + move[0], y + move[1]
-                if valid_range(cx, cy):
-                    cha = abs(arr[cx][cy] - arr[x][y])
-                    if not visited[cx][cy] and ((cha >= l) and (cha <= r)):
-                        visited[cx][cy] = 1
-                        varr.append((cx, cy))
-                        searched[cx][cy] = 1
-                        queue.append((cx, cy))
-                        c += 1
-                        p += arr[cx][cy]
-    return c, p
+            cx, cy = cur[0], cur[1]
+            for d in dirs:
+                dx, dy = cx + d[0], cy + d[1]
+                if valid_range(dx, dy) and not covered[dx][dy]:
+                    cha = abs(arr[dx][dy] - arr[cx][cy])
+                    if cha >= l and cha <= r:
+                        covered[dx][dy] = 1
+                        visited.append((dx, dy))
+                        queue.append((dx, dy))
+                        switch = True
+    return
 
 
-n, l, r = map(int, input().split())
 arr = []
-super_cnt = 0
+n, l, r = map(int, input().split())
 for _ in range(n):
     arr.append(list(map(int, input().split())))
+
+cnt = 0
 while True:
-    entry = []
-    searched = [[0]*n for _ in range(n)]
+    switch = False
+    all_parts = []
+    covered = [[0]*50 for _ in range(50)]
     for i in range(n):
         for j in range(n):
-            if not searched[i][j]:
-                visited = [[0]*n for _ in range(n)]
-                visited[i][j] = 1
-                varr = []
-                varr.append((i, j))
-                searched[i][j] = 1
-                queue = collections.deque([(i, j)])
-                cnt, p_sum = BFS()
-                if cnt == 0:
-                    continue
-                else:
-                    entry.append((int((p_sum + arr[i][j]) / (cnt + 1)), varr))
-    if entry:
-        for elem in entry:
-            for e in elem[1]:
-                arr[e[0]][e[1]] = elem[0]
-        super_cnt += 1
-    else:
+            covered[i][j] = 1
+            visited = [(i, j)]
+            queue = deque([(i, j)])
+            BFS()
+            if len(visited) > 1:
+                all_parts.append(visited)
+    for part in all_parts:
+        sum_v = 0
+        for elem in part:
+            sum_v += arr[elem[0]][elem[1]]
+        val = sum_v // len(part)
+        for elem in part:
+            arr[elem[0]][elem[1]] = val
+    if not switch:
         break
-
-print(super_cnt)
+    cnt += 1
+print(cnt)
